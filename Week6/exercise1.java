3391960a0a878cc2d0d6ef8bdf483ed4a4e99f80
JonahMiller-Lewis <jonahdmiller@lewisu.edu>
2020-08-10 11:32:20 -0500

    Create exercise1.java

---
+++ Week6/exercise1.java
@@ -0,0 +1,15 @@
+package Week6;
+
+public class exercise1 {
+
+		public static void method(int N) {
+			
+			for (int i = 0; i <= N; i++) {
+				for(int k = 0; k <= N; k++) {
+					for(int j = 0; j <= N; j++) {
+					}
+				}
+			}
+			
+		}
+}
